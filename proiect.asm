1437b8c43e0a755d3523c10bf16ad2d80b24a604
hrothgar32 <zold.almos@gmail.com>
2020-05-10 17:49:15 +0300

    Initial skeleton and documentation.

---
+++ proiect.asm
@@ -0,0 +1,17 @@
+    .386
+    .model flat, stdcall
+
+    includelib msvcrt.lib
+    extern exit: proc
+    extern printf: proc
+    extern scanf: proc
+
+    public start
+
+    .data
+
+    .code
+
+start:
+
+end start
