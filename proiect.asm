ba8bde066e11692385200b49b51fc6107a0e67c9
hrothgar32 <zold.almos@gmail.com>
2020-05-10 22:57:10 +0300

    Implemented user feedback loop.
    It is not yet debugged, due to OllyDbg acting funny.

--- proiect.asm
+++ proiect.asm
@@ -5,13 +5,44 @@
     extern exit: proc
     extern printf: proc
     extern scanf: proc
+    extern strcmp: proc
 
     public start
 
     .data
+    greeting_message db "Introduceti o expresie ", 0
+    format_string db "%s",0
+    user_input_string db 0
+    exit_string db "exit", 0
+    inital_variable dd 0
 
-    .code
 
+.code
 start:
+
+print_greeting_message:
+    push offset greeting_message
+    push offset format_string
+    call printf
+    add ESP, 8
+
+read_input:
+    push offset user_input_string
+    push offset format_string
+    call scanf
+    add ESP, 8
+
+check_if_exit:
+    push offset user_input_string
+    push offset exit_string
+    call strcmp
+    add ESP, 8
+    cmp EAX, 0
+    jne print_greeting_message
+
+
+    push 0
+    call exit
+
 
 end start
